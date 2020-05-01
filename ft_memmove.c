5df534e7224af601b93d8c97811c402a028d39f4
Diane <64610122+DianeHemi@users.noreply.github.com>
2020-05-01 18:05:41 +0200

    Correction of errors in variables names

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: dchampda <dchampda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 19:16:10 by dchampda          #+#    #+#             */
-/*   Updated: 2020/05/01 17:49:15 by dchampda         ###   ########.fr       */
+/*   Updated: 2020/05/01 17:54:25 by dchampda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (tmp_dest > tmp_src)
+	if (dest > src)
 	{
 		while (n > 0)
 		{
-			((char *)dest)[n - 1] = ((char *)src[n - 1]);
+			((char *)dest)[n - 1] = ((char *)src)[n - 1];
 			n--;
 		}
 	}
