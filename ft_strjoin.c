eb50bcf3b7a427c3499ff65536d8852d54f5befa
Diane <64610122+DianeHemi@users.noreply.github.com>
2020-05-07 23:13:25 +0200

    Changes on the while iterations

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dchampda <dchampda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 15:01:35 by dchampda          #+#    #+#             */
-/*   Updated: 2020/04/28 17:12:19 by dchampda         ###   ########.fr       */
+/*   Updated: 2020/05/07 23:09:27 by dchampda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*dest;
 	size_t	len;
 	size_t	i;
+	size_t	j;
 
 	len = ft_strlen(s1) + ft_strlen(s2);
 	if (!s1 || !s2)
@@ -24,15 +25,17 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (!(dest = malloc(sizeof(char) * len + 1)))
 		return (NULL);
 	i = 0;
-	while (*s1)
+	while (s1[i])
 	{
-		dest[i] = *s1++;
+		dest[i] = s1[i];
 		i++;
 	}
-	while (*s2)
+	j = 0;
+	while (s2[j])
 	{
-		dest[i] = *s2++;
+		dest[i] = s2[j];
 		i++;
+		j++;
 	}
 	dest[i] = '\0';
 	return (dest);
